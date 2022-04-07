49bc33074c90c75e9057ea784ba75f2f811b76c2
Dominik-Willaford <30693094+Dominik-Willaford@users.noreply.github.com>
2022-04-07 10:57:55 -0400

    Creating master that is a single page application that takes a form and adds the data to the database.

---
+++ CodeSamples/CodeSamples/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using CodeSamples.Models;
+using CodeSamples.Service;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeSamples.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IDatabaseFunctionService _databaseFunctionService;
+        public HomeController(IDatabaseFunctionService databaseFunctionService)
+        {
+            _databaseFunctionService = databaseFunctionService;
+        }
+        public ActionResult Index()
+        {            
+            return View(_databaseFunctionService.GetEntries());
+        }
+
+        [HttpPost]
+        public void AddUserToDB(UserInfo model)
+        {
+            _databaseFunctionService.CreateEntry(model);
+        }
+
+        [HttpPost]
+        public string GetUsers()
+        {
+            List<UserInfo> users = _databaseFunctionService.GetEntries();
+            return JsonConvert.SerializeObject(users);
+        }
+    }
+}
---
+++ CodeSamples/CodeSamples/Service/DatabaseFunctionService.cs
@@ -0,0 +1,34 @@
+﻿using CodeSamples.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeSamples.Service
+{
+    public interface IDatabaseFunctionService
+    {
+        void CreateEntry(UserInfo model);
+        List<UserInfo> GetEntries();
+    }
+    public class DatabaseFunctionService : IDatabaseFunctionService
+    {
+        private readonly DemoEntities _entities;
+
+        public DatabaseFunctionService(DemoEntities entities)
+        {
+            _entities = entities;
+        }
+
+        public void CreateEntry(UserInfo model)
+        {
+            _entities.UserInfoes.Add(model);
+            _entities.SaveChanges();
+        }
+
+        public List<UserInfo> GetEntries()
+        {
+            return _entities.UserInfoes.ToList();
+        }
+    }
+}
